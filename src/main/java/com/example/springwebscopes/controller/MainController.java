39e50c4c5dfc843c69895610033ce39f73110fe9
thanhnd <ducthanhnguyen95@gmail.com>
2023-05-28 16:13:29 +0700

    Using the application scope in a Spring web app

--- src/main/java/com/example/springwebscopes/controller/MainController.java
+++ src/main/java/com/example/springwebscopes/controller/MainController.java
@@ -1,6 +1,7 @@
 package com.example.springwebscopes.controller;
 
 import com.example.springwebscopes.scopes.LoggedUserManagementService;
+import com.example.springwebscopes.scopes.LoginCountService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,17 +10,21 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class MainController {
     private final LoggedUserManagementService loggedUserManagementService;
+    private final LoginCountService loginCountService;
 
-    public MainController(LoggedUserManagementService loggedUserManagementService) {
+    public MainController(LoggedUserManagementService loggedUserManagementService, LoginCountService loginCountService) {
         this.loggedUserManagementService = loggedUserManagementService;
+        this.loginCountService = loginCountService;
     }
 
     @GetMapping("/main")
     public String home(@RequestParam(required = false) String logout, Model model) {
         if (logout != null) loggedUserManagementService.setUsername(null);
         String username = loggedUserManagementService.getUsername();
+        int count = loginCountService.getCount();
         if (username == null) return "redirect:/";
         model.addAttribute("username", username);
+        model.addAttribute("loginCount", count);
         return "main.html";
     }
 }
